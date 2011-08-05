348f7fdf31e4d43754671b20b96e7f06158bf480
krnturcott <krnturcott@gmail.com>
2011-08-05 15:14:26 -0500

    Cambie core.php

--- classes/core.php
+++ classes/core.php
@@ -1,14 +1,14 @@
 <?php
 //SERVER
 /*
-define('DATABASE_SERVER', 'vansit0sdb.db.8066474.hostedresource.com');
-define('DATABASE_USER', 'vansit0sdb');
-define('DATABASE_PASSWORD', 'p4Nchit0sS');
-define('DATABASE_DB', 'vansit0sdb');
+define('DATABASE_SERVER', 'uniadidas.db.8100478.hostedresource.com');
+define('DATABASE_USER', 'uniadidas');
+define('DATABASE_PASSWORD', 'a4d1d4SUni');
+define('DATABASE_DB', 'uniadidas');
 */
 //LOCALHOST
-define('DATABASE_SERVER', 'vansit0sdb.db.8066474.hostedresource.com');
-define('DATABASE_USER', 'vansit0sdb');
-define('DATABASE_PASSWORD', 'p4Nchit0sS');
-define('DATABASE_DB', 'vansit0sdb');
+define('DATABASE_SERVER', 'localhost');
+define('DATABASE_USER', 'karen');
+define('DATABASE_PASSWORD', 'turcott');
+define('DATABASE_DB', 'karen');
 ?>
