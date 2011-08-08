d3d13c2d3db83422f83a1a2d2d2a82b754b36924
krnturcott <krnturcott@gmail.com>
2011-08-08 13:38:02 -0500

    agregue header.php

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<title>:: Universidad Virtual  ::</title>
+		<link rel="stylesheet" href="style.css" type="text/css">
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.2.min.js"></script>		
+		<script type="text/javascript" src="js/jquery.carouFredSel-4.3.3-packed.js"></script>	
+		<script src="js/swfobject_modified.js" type="text/javascript"></script>	
+		<script type="text/javascript" src="js/actions.js"></script>
+        	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<div id="logo">
+					<img src="imagesAdidas/logo.png" class="logo"/>
+					<img src="imagesAdidas/reebok-adidas.png" class="adidas-reebok"/>
+					
+					
+			  </div>	
+			  
+               <!--<h2 onclick="MM_goToURL('parent','perfil.php');return document.MM_returnValue">Perfil</h2>-->
+      </div>
---
+++ includes/header2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<title>::Universidad Virtual::</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<link rel="stylesheet"href="style.css" type="text/css">
+        	</head>
+	<div id="container">
+			<div id="header">
+				<div id="logo">
+					<img src="imagesAdidas/logo.png" class="logo"/>
+					<img src="imagesAdidas/adidasgroup.png" class="adidas-reebok"/>
+				</div>				
+			</div>
