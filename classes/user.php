38ecf7f14887d5fd0540b582a5862919ddade605
Wenceslao Negrete <vampediem@gmail.com>
2011-08-09 13:29:22 -0500

    user get_tipo_usuario, user set_image

--- classes/user.php
+++ classes/user.php
@@ -39,8 +39,23 @@ class User{
 		}
 	}
 	
+	public function get_tipo_usuario($id_user){
+		$user_result = $this->db->select_where("usuarios", "id_tipo_usuario", "id_usuario='$id_user'");
+		if(mysql_num_rows($user_result) > 0){
+			$user_tipo = mysql_fetch_array($user_result);
+			return $user_tipo['id_tipo_usuario'];
+		}else{
+			return false;
+		}
+	}
+	
+	public function set_image($id_user, $url_image){
+		$user_result = $this->db->update("usuarios", "image='$url_image'", "id_usuario='$id_user'");
+		return $user_result;
+	}
+	
 	public function profile($id_user){
-		$user_result = $this->db->select_where("usuarios", "id_usuario, email, password, id_tipo_usuario, nombre, id_sucursal, id_departamento, id_puesto", "id_usuario='$id_user'");
+		$user_result = $this->db->select_where("usuarios", "id_usuario, email, password, id_tipo_usuario, nombre, id_sucursal, id_departamento, id_puesto, image", "id_usuario='$id_user'");
 		if(mysql_num_rows($user_result )>0){
 			$user_profile = mysql_fetch_array($user_result);
 			$id_sucursal = $user_profile['id_sucursal'];
@@ -60,7 +75,7 @@ class User{
 			$puesto_array=mysql_fetch_array($puesto_result);
 			$puesto_nombre=$puesto_array['nombre'];
 			
-			return array('id_usuario'=>$user_profile['id_usuario'],'email'=>$user_profile['email'],'password'=>$user_profile['password'],'tipo_usuario'=>$user_profile['id_tipo_usuario'],'nombre'=>$user_profile['nombre'],'sucursal'=>$sucursal_nombre,'departamento'=>$departamento_nombre,'cadena'=>$cadena_nombre,'puesto'=>$puesto_nombre);
+			return array('id_usuario'=>$user_profile['id_usuario'],'email'=>$user_profile['email'],'password'=>$user_profile['password'],'tipo_usuario'=>$user_profile['id_tipo_usuario'],'nombre'=>$user_profile['nombre'], 'image'=>$user_profile['image'], 'sucursal'=>$sucursal_nombre,'departamento'=>$departamento_nombre,'cadena'=>$cadena_nombre,'puesto'=>$puesto_nombre);
 			
 			
 		}else{
