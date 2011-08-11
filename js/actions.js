8ec50d83476da339ad5127754d1ef446f1fd00e3
Wenceslao Negrete <vampediem@gmail.com>
2011-08-11 17:22:33 -0500

    tyc añadido a actions.js

--- js/actions.js
+++ js/actions.js
@@ -1,5 +1,8 @@
+function TyC() {
+	window.open( "tyc.html", "myWindow", "fullscreen=0, toolbar=0, location=0, status=0, menubar=0, scrollbars=0, resizable=0, width=900, height=900",1)
+}
+
 $(document).ready(function(){
-	
 	//---------- Perfil show imageupload form ----------------//
 	$('#profileImage').hide();
 	$('#default_foto').click(function(e){
