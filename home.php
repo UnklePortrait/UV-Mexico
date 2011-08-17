85cc61030d5d3374f690e8a5bd368a260b8f822d
Wenceslao Negrete <vampediem@gmail.com>
2011-08-17 13:57:34 -0500

    boton haz tu evaluacion ahora

--- home.php
+++ home.php
@@ -48,32 +48,32 @@ $user=profile();
 					</div>
 					<div id="swfHome">
 					<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="1000" height="480" id="Vid" title="adidas">
-        	  	<param name="movie" value="swf/Adidas_.swf" />
-        	  	<param name="quality" value="high" />
-        	  	<param name="wmode" value="transparent" />
-        	  	<param name="swfversion" value="6.0.65.0" />
-        	  	<!-- Esta etiqueta param indica a los usuarios de Flash Player 6.0 r65 o posterior que descarguen la versión más reciente de Flash Player. Elimínela si no desea que los usuarios vean el mensaje. -->
-        	  	<param name="expressinstall" value="Scripts/expressInstall.swf" />
-        	  	<!-- La siguiente etiqueta object es para navegadores distintos de IE. Ocúltela a IE mediante IECC. -->
-        	  	<!--[if !IE]>-->
-              	<object type="application/x-shockwave-flash" data="swf/Adidas_.swf" width="1000" height="480">
-        	    <!--<![endif]-->
-        	    <param name="quality" value="high" />
-        	    <param name="wmode" value="transparent" />
-        	    <param name="swfversion" value="6.0.65.0" />
-        	    <param name="expressinstall" value="Scripts/expressInstall.swf" />
-        	    <!-- El navegador muestra el siguiente contenido alternativo para usuarios con Flash Player 6.0 o versiones anteriores. -->
-        	    <div>
-        	      <h4>El contenido de esta página requiere una versión más reciente de Adobe Flash Player.</h4>
-        	      <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" width="112" height="33" /></a></p>
-      	      </div>
-        	    <!--[if !IE]>-->
-      	    	</object>
-        	 	<!--<![endif]-->
-      	  		</object>
-
+        	  			<param name="movie" value="swf/Adidas_.swf" />
+        	  			<param name="quality" value="high" />
+        	  			<param name="wmode" value="transparent" />
+        	  			<param name="swfversion" value="6.0.65.0" />
+        	  			<!-- Esta etiqueta param indica a los usuarios de Flash Player 6.0 r65 o posterior que descarguen la versión más reciente de Flash Player. Elimínela si no desea que los usuarios vean el mensaje. -->
+        	  			<param name="expressinstall" value="Scripts/expressInstall.swf" />
+                        <!-- La siguiente etiqueta object es para navegadores distintos de IE. Ocúltela a IE mediante IECC. -->
+                        <!--[if !IE]>-->
+                        <object type="application/x-shockwave-flash" data="swf/Adidas_.swf" width="1000" height="480">
+                        <!--<![endif]-->
+                        <param name="quality" value="high" />
+                        <param name="wmode" value="transparent" />
+                        <param name="swfversion" value="6.0.65.0" />
+                        <param name="expressinstall" value="Scripts/expressInstall.swf" />
+                        <!-- El navegador muestra el siguiente contenido alternativo para usuarios con Flash Player 6.0 o versiones anteriores. -->
+        				<div>
+        	      			<h4>El contenido de esta página requiere una versión más reciente de Adobe Flash Player.</h4>
+        	      			<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Obtener Adobe Flash Player" width="112" height="33" /></a></p>
+      	      			</div>
+        	    		<!--[if !IE]>-->
+      	    			</object>
+        	 		<!--<![endif]-->
+      	  			</object>
 					</div>
-				</div>					
+				</div>
+                <img class="personaje"  />					
 			</div>
 			<div class="clear"></div>
 			<div class="bottom3"></div>
